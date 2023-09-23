a51ba14d22a697ce07d09d9183a8d8c58a79b0c9
Yevhenii Tomberg <yevhenii@tomberg.com>
2023-09-23 03:51:28 +0300

    Initial commit

---
+++ src/main/java/tech/tomberg/forumut/ForumUtApplication.java
@@ -0,0 +1,13 @@
+package tech.tomberg.forumut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumUtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumUtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/tomberg/forumut/ForumUtApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tomberg.forumut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumUtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
